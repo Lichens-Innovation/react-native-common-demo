0b1c43c436bceef658b62404731caff6f49f49d2
André Masson <amwebexpert@gmail.com>
2026-03-11 10:56:03 -0600

    feat: rjsf paper implementation

--- src/components/rjsf/widgets/password-widget.tsx
+++ src/components/rjsf/widgets/password-widget.tsx
@@ -1,8 +1,7 @@
-import { useAppTheme } from '@lichens-innovation/react-native-common';
+import { useAppTheme, PasswordInput } from '@lichens-innovation/react-native-common';
 import type { WidgetProps } from '@rjsf/utils';
 import type { FunctionComponent } from 'react';
 import { StyleSheet } from 'react-native';
-import { TextInput } from 'react-native-paper';
 
 export const PasswordWidget: FunctionComponent<WidgetProps> = ({
   id,
@@ -25,14 +24,13 @@ export const PasswordWidget: FunctionComponent<WidgetProps> = ({
   const displayLabel = hideLabel ? undefined : (label ? `${label}${required ? ' *' : ''}` : undefined);
 
   return (
-    <TextInput
+    <PasswordInput
       mode="outlined"
       label={displayLabel}
       value={value ?? ''}
       placeholder={placeholder}
       disabled={disabled}
       editable={!readonly}
-      secureTextEntry
       onChangeText={(text) => onChange(text === '' ? options?.emptyValue : text)}
       onBlur={() => onBlur(id, value)}
       onFocus={() => onFocus(id, value)}
